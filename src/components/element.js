d33d8c6d5a2a0214fbc5e999acd44aed43ba7798
huohuo <18618110533@163.com>
2020-07-27 13:38:11 +0800

    引入布局

--- src/components/element.js
+++ src/components/element.js
@@ -1,9 +1,15 @@
-import { Button, Select } from "element-ui";
+import { Button, Row, Col, Container, Header, Main, Aside } from "element-ui";
 
 const element = {
   install: function(Vue) {
     Vue.component(Button.name, Button);
-    Vue.component(Select.name, Select);
+    Vue.component(Row.name, Row);
+    Vue.component(Container.name, Container);
+    Vue.component(Header.name, Header);
+    Vue.component(Main.name, Main);
+    Vue.component(Col.name, Col);
+    Vue.component(Col.name, Col);
+    Vue.component(Aside.name, Aside);
   },
 };
 
