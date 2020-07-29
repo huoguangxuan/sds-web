3292995c0e370fafe8aa32676d560794d0decdb0
huohuo <18618110533@163.com>
2020-07-29 10:09:40 +0800

    解决冲突

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>welcome11</div>
+  <el-main>
+    <h2>welcome请扫码登录</h2>
+    <qrcode :value="value" :size="size" level="H"></qrcode>
+  </el-main>
 </template>
+<script>
+export default {
+  data: function() {
+    return {
+      value: "www.baidu.com",
+      size: 300
+    };
+  }
+};
+</script>
+<style lang="less" scoped></style>
