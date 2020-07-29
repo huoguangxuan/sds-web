d1122638f3af929c7e6feb06fca11b7563521a76
zpf-github <976596115@qq.com>
2020-07-29 11:36:40 +0800

    修改

--- src/components/element.js
+++ src/components/element.js
@@ -1,14 +1,6 @@
-import {
-  Button,
-  Row,
-  Col,
-  Container,
-  Header,
-  Main,
-  Aside,
-  Progress
-} from "element-ui";
-
+import { Button, Row, Col, Container, Header, Main, Aside } from "element-ui";
+//生成二维码的组件 https://www.npmjs.com/package/qrcode.vue
+import QrcodeVue from "qrcode.vue";
 const element = {
   install: function(Vue) {
     Vue.component(Button.name, Button);
@@ -19,7 +11,7 @@ const element = {
     Vue.component(Col.name, Col);
     Vue.component(Col.name, Col);
     Vue.component(Aside.name, Aside);
-    Vue.component(Progress.name, Progress);
+    Vue.component("qrcode", QrcodeVue);
   }
 };
 
