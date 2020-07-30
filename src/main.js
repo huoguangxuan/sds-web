accca444ced57a66e84ef407a0650b1e94f6a7c7
luoyueyue <luoyueyue>
2020-07-30 09:33:50 +0800

    element组件报错

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/utils/permission";
-import element from "@components/element";
-import "element-ui/lib/theme-chalk/index.css";
+// import element from "@/components/element";
+// import "element-ui/lib/theme-chalk/index.css";
 
 // 引入全局重置样式
 import "./assets/base.css";
 
-Vue.use(element);
+// Vue.use(element);
 
 Vue.config.productionTip = false;
 
