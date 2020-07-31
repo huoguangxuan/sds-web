ca4800345b7b183fb3cad31169daf48ef05097f8
luoyueyue <luoyueyue>
2020-07-31 14:28:41 +0800

    1.增加充值页面2.添加充值成功图片3.添加相对应路由4.增加路由白名单

--- src/router/index.js
+++ src/router/index.js
@@ -79,6 +79,13 @@ let routes = [
     name: "recharging",
     component: () =>
       import(/* webpackChunkName: "pages" */ "../views/recharge/recharging")
+  },
+  // 充值结束
+  {
+    path: "/rechargeOk",
+    name: "rechargeOk",
+    component: () =>
+      import(/* webpackChunkName: "pages" */ "../views/recharge/rechargeOk")
   }
 ];
 
