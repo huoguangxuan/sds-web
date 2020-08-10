b25d46af828c9d510eb273a7e672fb793040b392
zpf-github <976596115@qq.com>
2020-08-10 09:37:03 +0800

    解决APP.vue中的冲突

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <!-- 公共头部 -->
-    <!-- <public-head ref="head"></public-head> -->
-=======
->>>>>>> ff1e6aeac3d3cfbf5bdf17adf4d5412a12b1808c
     <!-- 内容区域 -->
     <!-- <div class="contentArea"> -->
       <router-view></router-view>
